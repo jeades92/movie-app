83413a7d9926392c57fccddf60a764e1af0542ae
Jimmys Mac <eadesj25@gmail.com>
2023-05-18 14:21:59 -0400

    login and signup shtyle

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -11,43 +11,42 @@ const Header = () => {
   };
 
   return (
-    <Box bg="info" color="dark" mb={4} py={3} display="flex" alignItems="center">
-      <Box
-        className="container"
-        display="flex"
-        flexDirection="column"
-        justifyContent={{ base: "center", lg: "space-between" }}
-        alignItems="center"
-        textAlign="center"
-      >
+    <Box
+      bg="teal.500"
+      color="white"
+      py={4}
+      px={6}
+      rounded="md"
+      display="flex"
+      alignItems="center"
+      justifyContent="space-between"
+    >
+      <Box flex="1" textAlign="center">
         <Link to="/">
           <Heading as="h1" fontSize={{ base: "3xl", lg: "6xl" }} m={0}>
-            Movies
+            Movie-App
           </Heading>
         </Link>
-        <Text fontSize={{ base: "xl", lg: "3xl" }} fontWeight="bold" m={0}>
-          Movie Reviews
-        </Text>
-        <Box>
-          {Auth.loggedIn() ? (
-            <Button variant="outline" size="lg" colorScheme="teal" m={2} onClick={logout}>
-              Logout
-            </Button>
-          ) : (
-            <>
-              <Link to="/login">
-                <Button variant="solid" size="lg" colorScheme="teal" m={2}>
-                  Login
-                </Button>
-              </Link>
-              <Link to="/signup">
-                <Button variant="outline" size="lg" colorScheme="teal" m={2}>
-                  Signup
-                </Button>
-              </Link>
-            </>
-          )}
-        </Box>
+      </Box>
+      <Box>
+        {Auth.loggedIn() ? (
+          <Button variant="outline" size="sm" colorScheme="whiteAlpha" m={2} onClick={logout}>
+            Logout
+          </Button>
+        ) : (
+          <>
+            <Link to="/login">
+              <Button variant="solid" size="sm" colorScheme="whiteAlpha" m={2}>
+                Login
+              </Button>
+            </Link>
+            <Link to="/signup">
+              <Button variant="outline" size="sm" colorScheme="whiteAlpha" m={2}>
+                Signup
+              </Button>
+            </Link>
+          </>
+        )}
       </Box>
     </Box>
   );
